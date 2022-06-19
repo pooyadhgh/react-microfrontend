884705f6d0339526c86855d22a7015a835fa0628
Pooya Dehghani <pooya.dehghani@hm.com>
2022-06-19 21:05:24 +0200

    Connect marketing to container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
+const { ModuleFederationPlugin } = require('webpack').container;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -13,6 +15,12 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      remotes: {
+        '@marketing': 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import MarketingApp from './components/MartketingApp';
 
 const App = () => {
-  return <h1>Container App</h1>;
+  return (
+    <>
+      <h1>Container App</h1>
+      <MarketingApp />
+    </>
+  );
 };
 
 export default App;
