2e395e890260725eec089f65e3e56737fe2d8888
Pooya Dehghani <pooya.dehghani@hm.com>
2022-06-26 12:30:28 +0200

    Fix css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import MarketingApp from './components/MartketingApp';
 import Header from './components/Header';
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'container',
+});
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <MarketingApp />
+        <StylesProvider generateClassName={generateClassname}>
+          <Header />
+          <MarketingApp />
+        </StylesProvider>
       </BrowserRouter>
     </>
   );
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'marketing',
+});
+
 export default () => {
   return (
     <>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassname}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
