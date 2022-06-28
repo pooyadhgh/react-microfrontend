64042761c6c63eb0f17c23f89a8c905c02c665c1
Pooya Dehghani <pooya.dehghani@hm.com>
2022-06-28 17:01:07 +0200

    Implement shared routing history between apps

--- packages/container/src/components/MartketingApp.js
+++ packages/container/src/components/MartketingApp.js
@@ -1,11 +1,24 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from '@marketing/MarketingApp';
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
+  const {
+    location: { pathname },
+  } = history;
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref}></div>;
