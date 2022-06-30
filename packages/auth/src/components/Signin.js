b0c48df6e25020a7fa2549be564ea131c06d3978
Pooya Dehghani <pooya.dhgh@gmail.com>
2022-06-30 15:04:34 +0200

    Update Signin.js

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -15,7 +15,7 @@ import { Link } from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright '}
       <Link color="inherit" to="/">
         Your Website
       </Link>{' '}
