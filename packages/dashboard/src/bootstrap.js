39ef9c7f8966be82187a46e23db5666c62894745
Pooya Dehghani <pooya.dehghani@hm.com>
2022-07-01 10:30:56 +0200

    Add dashboard app

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+export const mount = (element) => {
+  const app = createApp(Dashboard);
+  app.mount(element);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_dashboard-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
